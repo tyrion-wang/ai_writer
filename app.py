363642b81bc687117295c1576bb81e1617ab1de4
枫子 <maple_leaf_7@msn.com>
2023-04-08 11:53:28 +0800

    Revert "first commit"
    This reverts commit 6f073824ac92c73278ca2cff9ee14a6d9d820ec6.

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+import os
+import json
+
+# 配置openai的API Key
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+# 初始化Flask
+app = Flask(__name__)
+
+
+# 定义首页
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# 定义转写函数
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    # 获取用户输入的文字
+    text = request.form['text']
+
+    # 获取用户选择的相似度
+    similarity = request.form['similarity']
+    temperature = 1.0 - float(similarity) / 10.0
+
+    # 使用openai的GPT-3模型进行文本加工
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=text,
+        temperature=temperature,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # 提取GPT-3的输出文本
+    transcription = response.choices[0].text
+
+    # 返回json格式的结果
+    return jsonify({'transcription': transcription.strip()})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
